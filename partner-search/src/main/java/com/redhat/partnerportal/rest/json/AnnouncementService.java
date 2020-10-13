f4875b38b8e505ea0c5a41bc6486e25dc695911e
leaswaran1976 <leaswaran@live.com>
2020-10-13 17:12:48 -0400

    Changed Date format

--- partner-search/src/main/java/com/redhat/partnerportal/rest/json/AnnouncementService.java
+++ partner-search/src/main/java/com/redhat/partnerportal/rest/json/AnnouncementService.java
@@ -14,6 +14,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+
 import io.quarkus.panache.common.Sort;
 
 @Path("/announcements")
@@ -21,6 +23,8 @@ import io.quarkus.panache.common.Sort;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AnnouncementService {
+	public static final Logger logger = Logger.getLogger(AnnouncementService.class);
+
 	@GET
 	public List<Announcements> list() {
 		return Announcements.listAll(Sort.by("end_date"));
@@ -30,11 +34,12 @@ public class AnnouncementService {
 	@Path("/active")
 	public List<Announcements> getActiveAnnouncements() {
 		Stream<Announcements> sls = Announcements.streamAll();
-		SimpleDateFormat sfd = new SimpleDateFormat("yyyy-MM-dd");
+		SimpleDateFormat sfd = new SimpleDateFormat("MM/dd/yyyy");
 		sls = sls.filter(p -> {
 			try {
 				return sfd.parse(p.end_date).after(sfd.parse(sfd.format(new Date())));
 			} catch (ParseException e) {
+				logger.fatal(e);
 				return false;
 			}
 		});
